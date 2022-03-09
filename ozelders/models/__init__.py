ff1320e1adc3f78cc85127e9bbd130b8414afb29
badl7 <btlgrbz071@gmail.com>
2022-03-09 10:38:44 +0300

    errors daki hintler önemli

--- ozelders/models/__init__.py
+++ ozelders/models/__init__.py
@@ -2,5 +2,6 @@ from .kategori import KategoriModel
 from .ilan import IlanlarModel
 from .basvur import BasvurModel
 from .iletisim import IletisimModel
+from .user import User, Student, Teacher
 
 
