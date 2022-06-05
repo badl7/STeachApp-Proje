6a6bb49172af43144a91fd47c63d013f88d2e546
badl7 <btlgrbz071@gmail.com>
2022-06-05 14:54:08 +0300

    detay

--- ozelders/views/kategori.py
+++ ozelders/views/kategori.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from ozelders.models import IlanlarModel
+from ozelders.models import IlanlarModel, KategoriModel
 from django.core.paginator import Paginator
 
 
 def kategori(request, kategoriSlug):
-    kategori = get_object_or_404
-    print(kategoriSlug)
-    ilanlar = IlanlarModel.objects.all()
+    kategori = get_object_or_404(KategoriModel, slug=kategoriSlug)
+    ilanlar = kategori.ilan.order_by('-id')
+    print(ilanlar)
     sayfa = request.GET.get('sayfa')
     paginator = Paginator(ilanlar, 3)
 
